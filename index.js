0bc0b2be9715fc22b182528896fdfd0679ad9d5e
Xenia <kakirilyuk@gmail.com>
2022-04-10 17:13:05 -0400

    commit tree d8111d4ad3789219f272ad9db3491c560ff29110

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+
+
+let totalBatteries = batteryBatches.reduce(function (previousValue, currentValue) {
+    return previousValue + currentValue
+  }, 0)
+
+
